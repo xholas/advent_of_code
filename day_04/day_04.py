1609886f9b601e6afa17135a0a7183cf9c47d691
Jan Holásek <xholasek@gmail.com>
2022-12-04 13:17:37 +0100

    Day 04 - part 2

--- day_04/day_04.py
+++ day_04/day_04.py
@@ -18,9 +18,25 @@ class Range:
     def does_contain(self, other):
         if not isinstance(other, Range):
             raise Exception('Method <' + Range.does_contain.__name__ + '> can only compare Range with other Range.')
+        # Fully contained
+        # Other starts after self and ends before self (any border may be equal)
+        # Self:  -12345-
+        # Other: ---345-
         if self.start <= other.start and other.end <= self.end:
             return True
         return False
+
+    def does_overlap(self, other):
+        if not isinstance(other, Range):
+            raise Exception('Method <' + Range.does_overlap.__name__ + '> can only compare Range with other Range.')
+        # No overlap
+        # One ends before other starts
+        # Self:  -123---    or  -----45-
+        # Other: ----45-        -123----
+        if self.end < other.start or other.end < self.start:
+            return False
+        # Everything else is overlap (including one fully contained in the other)
+        return True
 
 
 def debug(*args, **kwargs):
@@ -31,6 +47,7 @@ def debug(*args, **kwargs):
 def main():
     with open('input_day_04.txt') as file:
         full_overlaps_cnt = 0
+        any_overlaps_cnt = 0
         for line in file:
             line = line.strip()
             debug(line)
@@ -44,10 +61,16 @@ def main():
             debug("\t" + str(second))
             # Is fully overlapping?
             is_overlap = first.does_contain(second) or second.does_contain(first)
-            debug("\tOverlapping: " + str(is_overlap))
+            debug("\tFull overlap: " + str(is_overlap))
             if is_overlap:
                 full_overlaps_cnt += 1
+            # Is any overlap?
+            is_overlap = first.does_overlap(second)
+            debug("\tAny overlap: " + str(is_overlap))
+            if is_overlap:
+                any_overlaps_cnt += 1
     print('Fully overlapping ranges: ' + str(full_overlaps_cnt))
+    print('All overlapping ranges: ' + str(any_overlaps_cnt))
 
 
 if __name__ == '__main__':
