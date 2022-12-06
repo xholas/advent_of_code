d891e72f798eabf197e01ffbf2ce9597efb7ddd1
Jan Holásek <xholasek@gmail.com>
2022-12-06 21:32:05 +0100

    Day 06 - part 2

--- day_06/day_06.py
+++ day_06/day_06.py
@@ -2,12 +2,19 @@ import re
 import sys
 
 # Print debug messages
-DEBUG = True
+DEBUG = False
 
 
 def debug(*args, **kwargs):
     if DEBUG:
         print(*args, file=sys.stderr, **kwargs)
+
+
+def get_pattern_for_len(length):
+    # debug(length)
+    if length == 1:
+        return '(.)'
+    return get_pattern_for_len(length - 1) + '(?!' + '|'.join(['\\' + str(x) for x in range(1, length)]) + ')(.)'
 
 
 def main():
@@ -21,6 +28,17 @@ def main():
     # Get the length
     length = sequence_match.span()[1]
     print('Chars in buffer to the end of starting marker: ' + str(length))
+
+    # Part 2
+    # Create regex pattern for 14 distinct character sequence
+    # (Yes, I am sick of this myself. Please, forgive me.)
+    pattern = get_pattern_for_len(14)
+    debug(pattern)
+    # Find the match
+    match = re.search(pattern, buffer)
+    debug(match)
+    # Print the length
+    print('Just take the number and stop this evil code ASAP: ' + str(match.span()[1]))
 
 
 if __name__ == '__main__':
