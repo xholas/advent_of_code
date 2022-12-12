e718d828f4075addd532157be445f511bd376658
Jan Holásek <xholasek@gmail.com>
2022-12-12 22:28:44 +0100

    Day 11 - part 2

--- day_11/README.md
+++ day_11/README.md
@@ -275,3 +275,157 @@ Figure out which monkeys to chase by counting
 how many items they inspect over 20 rounds.  
 **_What is the level of monkey business 
 after 20 rounds of stuff-slinging simian shenanigans?_**
+
+
+# Part Two 
+
+You're worried you might not ever get your items back. 
+So worried, in fact, that your relief 
+that a monkey's inspection didn't damage an item 
+no longer causes your worry level to be divided by three.
+
+Unfortunately, that relief was all 
+that was keeping your worry levels from reaching ridiculous levels. 
+You'll need to find another way to keep your worry levels manageable.
+
+At this rate, you might be putting up with these monkeys 
+for a very long time - possibly 10000 rounds!
+
+With these new rules, 
+you can still figure out the monkey business after 10000 rounds. 
+Using the same example above:
+
+```
+== After round 1 ==
+Monkey 0 inspected items 2 times.
+Monkey 1 inspected items 4 times.
+Monkey 2 inspected items 3 times.
+Monkey 3 inspected items 6 times.
+
+== After round 20 ==
+Monkey 0 inspected items 99 times.
+Monkey 1 inspected items 97 times.
+Monkey 2 inspected items 8 times.
+Monkey 3 inspected items 103 times.
+
+== After round 1000 ==
+Monkey 0 inspected items 5204 times.
+Monkey 1 inspected items 4792 times.
+Monkey 2 inspected items 199 times.
+Monkey 3 inspected items 5192 times.
+
+== After round 2000 ==
+Monkey 0 inspected items 10419 times.
+Monkey 1 inspected items 9577 times.
+Monkey 2 inspected items 392 times.
+Monkey 3 inspected items 10391 times.
+
+== After round 3000 ==
+Monkey 0 inspected items 15638 times.
+Monkey 1 inspected items 14358 times.
+Monkey 2 inspected items 587 times.
+Monkey 3 inspected items 15593 times.
+
+== After round 4000 ==
+Monkey 0 inspected items 20858 times.
+Monkey 1 inspected items 19138 times.
+Monkey 2 inspected items 780 times.
+Monkey 3 inspected items 20797 times.
+
+== After round 5000 ==
+Monkey 0 inspected items 26075 times.
+Monkey 1 inspected items 23921 times.
+Monkey 2 inspected items 974 times.
+Monkey 3 inspected items 26000 times.
+
+== After round 6000 ==
+Monkey 0 inspected items 31294 times.
+Monkey 1 inspected items 28702 times.
+Monkey 2 inspected items 1165 times.
+Monkey 3 inspected items 31204 times.
+
+== After round 7000 ==
+Monkey 0 inspected items 36508 times.
+Monkey 1 inspected items 33488 times.
+Monkey 2 inspected items 1360 times.
+Monkey 3 inspected items 36400 times.
+
+== After round 8000 ==
+Monkey 0 inspected items 41728 times.
+Monkey 1 inspected items 38268 times.
+Monkey 2 inspected items 1553 times.
+Monkey 3 inspected items 41606 times.
+
+== After round 9000 ==
+Monkey 0 inspected items 46945 times.
+Monkey 1 inspected items 43051 times.
+Monkey 2 inspected items 1746 times.
+Monkey 3 inspected items 46807 times.
+
+== After round 10000 ==
+Monkey 0 inspected items 52166 times.
+Monkey 1 inspected items 47830 times.
+Monkey 2 inspected items 1938 times.
+Monkey 3 inspected items 52013 times.
+```
+
+After 10000 rounds, 
+the two most active monkeys inspected items `52166` and `52013` times. 
+Multiplying these together, 
+the level of monkey business in this situation is now `2713310158`.
+
+Worry levels are **no longer divided by three** after each item is inspected; 
+you'll need to find another way to **keep your worry levels manageable**.
+Starting again from the initial state in your puzzle input,  
+**_what is the level of monkey business after 10000 rounds?_**
+
+
+### Notes
+
+See: [Modular arithmetic](https://en.wikipedia.org/wiki/Modular_arithmetic)
+
+* `a + k ≡ b + k (mod n)` for any integer `k` (compatibility with translation)
+<a name="ma-translation">(1)</a>
+
+* `k*a ≡ k*b (mod n)` for any integer `k` (compatibility with scaling)
+<a name="ma-scaling">(2)</a>
+
+* `a^k ≡ b^k (mod n)` for any non-negative integer `k` (compatibility with exponentiation)
+<a name="ma-exponentiation">(3)</a>
+
+
+---
+
+The numbers (worry levels) can change in 3 ways:
+
+- add some number `A`
+- multiply by number `M`
+- multiply by itself `x^2` (where `x` is old value)
+
+
+---
+
+Testing is done for any number `X`
+and tests truthfulness of expression `X mod N = 0` for some number `N`.
+
+Any number `X` will have the same test result as `X mod N`.  
+(`X mod N = (X mod N) mod N` <=> `X ≡ X mod N (mod N)`)
+
+* Adding any number to `X` will produce the same effect 
+as adding the same number to `X mod N` [[1]](#ma-translation)
+* Multiplying `X` by any number is equivalent to multiplying `X mod N`
+[[2]](ma-scaling)
+* Multiplying `X` by itself will have the same effect as with `X mod N`
+[[3]](ma-exponentiation)
+
+
+---
+
+I need to find such number `N`, 
+that in **arithmetic modulo N** 
+all the tests for each monkey's testing value:  
+`m0, m1, ... m(monkey_count-1)`  
+will give correct result.  
+That is for any `X; X mod mM = 0` the `X mod N mod mM` will also give `0`.
+
+Basically `N` has to be multiple of each `mM`.
---
+++ day_11/day_11_part2.py
@@ -0,0 +1,217 @@
+import sys
+
+# Print debug messages
+DEBUG = False
+
+# Set this in initialization
+LEAST_COMMON_MULTIPLE = None
+
+
+def debug(*args, **kwargs):
+    if DEBUG:
+        print(*args, file=sys.stderr, **kwargs)
+
+
+class Monkey:
+    def __init__(self,
+                 starting_items,
+                 operation_type,
+                 operation_amount,
+                 test_divisible,
+                 target_true,
+                 target_false,
+                 monkeys_list):
+        if not isinstance(starting_items, list):
+            raise Exception('Items must be a list of numbers.')
+        self.items = starting_items
+        self.operation = operation_type
+        self.operation_amount = operation_amount
+        self.test_divisible = test_divisible
+        self.target_true = target_true
+        self.target_false = target_false
+        self.monkeys_list = monkeys_list
+
+        self.inspected_items = 0
+
+    def __str__(self):
+        return 'Monkey ' + str(next(idx for idx, monke in enumerate(self.monkeys_list) if monke is self)) \
+               + ' with ' + str(len(self.items)) + ' items. \n' \
+               + 'Testing divisibility by ' + str(self.test_divisible) \
+               + ' - then throwing to ' + str(self.target_true) + ' or ' + str(self.target_false) + '. \n' \
+               + 'You get worried ' + self.operation + ' ' + self.operation_amount + ' every time it picks up an item.'
+
+    def inspect(self, worry_level):
+        # Count inspected items
+        self.inspected_items += 1
+
+        # What is the operation value?
+        if not isinstance(self.operation_amount, str):
+            raise Exception('Operation amount must be stored as a string.')
+        if self.operation_amount.isnumeric():
+            operation_value = int(self.operation_amount)
+        elif self.operation_amount == 'old':
+            operation_value = worry_level
+        else:
+            raise Exception('Unknown operation amount <' + self.operation_amount + '>')
+
+        # Based on type, how does the worry level change?
+        if self.operation == '+':
+            return worry_level + operation_value
+        elif self.operation == '*':
+            return worry_level * operation_value
+        else:
+            raise Exception('Unknown operation type <' + self.operation + '>')
+
+    def test(self, worry_level):
+        # What is the target based on the divisibility test?
+        if worry_level % self.test_divisible == 0:
+            return self.target_true
+        else:
+            return self.target_false
+
+    def turn(self):
+        # Turn is when monkey inspects and throws all its items
+        while len(self.items) != 0:  # Must not loop over items in changing list
+            # Inspect item
+            # - take item from list and see the worry level
+            # debug('-------------------------------------------------')
+            # debug('Monke has: ' + str(self.items))
+            item_worry_level = self.items.pop(0)  # Really? pop() pops last item? not first?
+            # debug('Monke gets: ' + str(self.items))
+            # - raise worry level based on the operation
+            item_worry_level = self.inspect(item_worry_level)
+
+            # In part 2 this no longer works
+            # # Relief
+            # # - the relief causes the worry level to be divided by three and rounded down
+            # item_worry_level = item_worry_level // 3
+
+            # Modular arithmetics will save us from big numbers
+            item_worry_level = item_worry_level % LEAST_COMMON_MULTIPLE
+
+            # Test worry level
+            target_monkey_idx = self.test(item_worry_level)
+            target_monkey = self.monkeys_list[target_monkey_idx]
+            if not isinstance(target_monkey, Monkey):
+                raise Exception('Monkey! Are you with us?!')
+
+            # Throw an item
+            # debug('-------------------------------------------------')
+            # debug('Monke has: ' + str(target_monkey.items))
+            target_monkey.items.append(item_worry_level)
+            # debug('Monke gets: ' + str(target_monkey.items))
+
+
+def setup_monkey(monkey_info, monkeys_list):
+    monkey_name = monkey_info[0].split()
+    monkey_number = int(monkey_name[1][:1])
+    debug('Monkey number: ' + str(monkey_number))
+
+    monkey_items = [int(x.strip(',')) for x in monkey_info[1].split()[2:]]
+    debug('Monkey items: ' + str(monkey_items))
+
+    monkey_operation = monkey_info[2].split()
+    monkey_operation_type = monkey_operation[4]
+    monkey_operation_value = monkey_operation[5]
+    debug('Monkey operation is <' + monkey_operation_type + '> with value <' + monkey_operation_value + '>')
+
+    monkey_test_divisible = int(monkey_info[3].split()[3])
+    debug('Monkey tests divisibility by: ' + str(monkey_test_divisible))
+
+    monkey_target_true = int(monkey_info[4].split()[-1])
+    monkey_target_false = int(monkey_info[5].split()[-1])
+    debug('Monkey throws items to <' + str(monkey_target_true) + '> if condition is true, '
+          'otherwise to <' + str(monkey_target_false) + '>')
+
+    if monkey_number != len(monkeys_list):
+        raise Exception('Monkeys order is not correct. '
+                        'Next monkey index should be <' + str(monkey_number) + '> '
+                        'but is <' + str(len(monkeys_list)) + '>')
+
+    monkeys_list.append(Monkey(
+        monkey_items,
+        monkey_operation_type,
+        monkey_operation_value,
+        monkey_test_divisible,
+        monkey_target_true,
+        monkey_target_false,
+        monkeys_list
+    ))
+
+
+def main():
+    monkeys = []
+    # with open('input_test.txt') as file_input:
+    with open('input_day_11.txt') as file_input:
+        input_all = file_input.read()
+
+    # Split per Monkey
+    input_per_monkey = input_all.split('\n\n')
+    for monkey_info_str in input_per_monkey:
+        # debug('START')
+        # debug(monkey_info_str)
+        # debug('STOP\n\n')
+        monkey_info = monkey_info_str.split('\n')
+        debug('\nInfo: ' + str(monkey_info))
+
+        setup_monkey(monkey_info, monkeys)
+
+    debug('\nThere are ' + str(len(monkeys)) + ' monkeys. Last is:')
+    debug(monkeys[-1])
+
+    # Set the LEAST_COMMON_MULTIPLE
+    # https://stackoverflow.com/a/42472824
+    from math import gcd
+    a = [monke.test_divisible for monke in monkeys]  # will work for an int array of any length
+    lcm = 1
+    for i in a:
+        lcm = lcm * i // gcd(lcm, i)
+    global LEAST_COMMON_MULTIPLE
+    LEAST_COMMON_MULTIPLE = lcm
+
+    max_rounds = 10000
+    # Monkeys play for 20 rounds
+    for current_round in range(max_rounds):
+        # Round is when all monkeys take one turn
+        for monkey in monkeys:
+            if not isinstance(monkey, Monkey):
+                raise Exception('This is no monkey!')
+            # Turn is when monkey inspects and throws all its items
+            monkey.turn()
+
+        # # Debug part 2 - compare result with test data
+        # if current_round + 1 in [1, 20, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
+        #     global DEBUG
+        #     DEBUG = True
+        #     debug('== After round ' + str(current_round + 1) + ' ==')
+        #     for num, monkey in enumerate(monkeys):
+        #         debug('Monkey ' + str(num) + ' inspected items ' + str(monkey.inspected_items) + ' times.')
+        #     DEBUG = False
+
+    debug('\nThere are ' + str(len(monkeys)) + ' monkeys. \nFirst is:')
+    debug(monkeys[1])
+
+    debug('\nAnd last is:')
+    debug(monkeys[-1])
+
+    debug('\nNuber of inspections:')
+    for idx, monkey in enumerate(monkeys):
+        debug('Monkey ' + str(idx) + ': ' + str(monkey.inspected_items))
+
+    monkey_activity = [monke.inspected_items for monke in monkeys]
+    # debug(monkey_activity)
+
+    # First most active monkey
+    top_activity = max(monkey_activity)
+
+    # Second most active monkey
+    monkey_activity.remove(top_activity)
+    second_activity = max(monkey_activity)
+
+    # Monkey business level
+    monkey_business_level = top_activity * second_activity
+    print('The level of monkey business is ' + str(monkey_business_level))
+
+
+if __name__ == '__main__':
+    main()
